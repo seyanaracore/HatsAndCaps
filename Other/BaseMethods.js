5bac44ca45d766ccb1c0110a1fa8fda663b87777
HatsAndCaps <hello@goorin.ru>
2022-01-20 17:00:37 +0300

    csv update

--- Other/BaseMethods.js
+++ Other/BaseMethods.js
@@ -1,41 +1,82 @@
 window.sleep = (sec = 0.5) => {
-    return new Promise((res) => {
-      setTimeout(() => res(), sec * 1000);
-    });
-  };
-  window.toBottomElement = async (elementSelector = "html", offset = 0) => {
-    const element = document.querySelector(elementSelector);
-    return new Promise(async (res) => {
-      let actualScroll = 1;
-      let maxScroll = 0;
-      while (actualScroll != maxScroll) {
-        await sleep(0.5);
-        actualScroll = element.scrollTop;
-        element.scrollTop += 9999;
-        maxScroll = element.scrollTop;
-      }
-      element.scrollTop -= offset;
-      res();
-    });
-  };
-  window.copyToClipboard = (string) => {
-    let area = document.createElement("textarea");
-    document.body.appendChild(area);
-    area.value = string;
-    area.select();
-    document.execCommand("copy");
-    document.body.removeChild(area);
-    console.log("\ncopied");
-  };
-  window.findSrollingElement = (selector = "div") => {
-    document
-      .querySelectorAll(selector)
-      .forEach((e) => e.addEventListener("scroll", (e) => console.log(e)));
-  };
-  window.downloadData = (dataString, fileName, fileFormat) => {
-    let a = document.createElement("a");
-    a.href = dataString;
-    a.setAttribute("download", fileName + "." + fileFormat);
-    a.click();
-  };
-  
+  return new Promise((res) => {
+    setTimeout(() => res(), sec * 1000);
+  });
+};
+window.toBottomElement = async (elementSelector = "html", offset = 0) => {
+  const element = document.querySelector(elementSelector);
+  return new Promise(async (res) => {
+    let actualScroll = 1;
+    let maxScroll = 0;
+    while (actualScroll != maxScroll) {
+      await sleep(0.5);
+      actualScroll = element.scrollTop;
+      element.scrollTop += 9999;
+      maxScroll = element.scrollTop;
+    }
+    element.scrollTop -= offset;
+    res();
+  });
+};
+window.copyToClipboard = (string) => {
+  let area = document.createElement("textarea");
+  document.body.appendChild(area);
+  area.value = string;
+  area.select();
+  document.execCommand("copy");
+  document.body.removeChild(area);
+  console.log("\ncopied");
+};
+window.createCSV = ({ content, headers, sep = ";"} = content) => {
+  let fileData = `sep=${sep}\n`;
+
+  //Заголовки
+  if (headers.length) fileData += Array.isArray(headers) ? `${headers.join(sep)}\n` : `${headers}\n`
+
+  //Содержание
+  content.forEach((item) => {
+    //Массив объектов
+    for (let value of Object.values(item)) {
+      //1 объект = 1 строка
+      fileData += `${value}${sep}`; //1 свойство = 1 столбец
+    }
+    fileData += `\n`;
+  });
+  return fileData;
+};
+window.download = (content, fileName, fileFormat) => {
+  switch (fileFormat) {
+    case "txt":
+      content = "data:text/plain;charset=utf-8," + encodeURIComponent(content);
+      break;
+    case "csv":
+      content =
+        "data:text/plain;charset=utf-8," +
+        encodeURIComponent(window.createCSV(content));
+      break;
+  }
+
+  let a = document.createElement("a");
+  a.style.display = "none";
+  a.href = content;
+  a.setAttribute("download", fileName + "." + fileFormat);
+  a.click();
+};
+window.LocalStorageUtil = {
+  get(key = null) {
+    if (!key) return;
+    try {
+      return JSON.parse(localStorage.getItem(key));
+    } catch (error) {
+      console.error(error);
+    }
+  },
+  set(key = null, value) {
+    if (!key) return;
+    localStorage.setItem(key, JSON.stringify(value));
+  },
+  delete(key = null) {
+    if (!key) return;
+    localStorage.removeItem(key);
+  },
+};
