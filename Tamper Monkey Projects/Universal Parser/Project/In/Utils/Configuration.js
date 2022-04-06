b0e4599cc0cc02e8b86c4a2818299e6dac43ccf3
Anton Tigunov <seyanaracore@gmail.com>
2022-04-06 14:41:08 +0300

    Configuration console info

--- Tamper Monkey Projects/Universal Parser/Project/In/Utils/Configuration.js
+++ Tamper Monkey Projects/Universal Parser/Project/In/Utils/Configuration.js
@@ -1,24 +1,27 @@
 class Configuration {
-	static #config = {
-		itemsLinksListName: "itemsLinksList",
-		itemsInfoListName: "itemsInfoList",
-		itemsErrorsListName: "itemsErrorsList",
-		initialPage: "",
-		sleepTime: 3,
-		writePageUrl: true,
-		validatePageUrl: false,
-	};
+   static #config = {
+      itemsLinksListName: "itemsLinksList",
+      itemsInfoListName: "itemsInfoList",
+      itemsErrorsListName: "itemsErrorsList",
+      initialPage: "",
+      sleepTime: 3,
+      writePageUrl: true,
+      validatePageUrl: false,
+   };
 
-	static set(configObject) {
-		if (configObject && typeof configObject !== "object") throw new Error("Excepted settings object")
+   static set(configObject) {
+      if (configObject && typeof configObject !== "object") {
+         throw new Error("Excepted settings object");
+      }
 
-		if (configObject) {
-			for (let prop in configObject) {
-				this.#config[prop] = configObject[prop]
-			}
-		}
-	}
-	static get = () => this.#config
+      if (configObject) {
+         for (let prop in configObject) {
+            this.#config[prop] = configObject[prop];
+         }
+         console.log(`Config setted: ${this.get()}`);
+      }
+   }
+   static get = () => this.#config;
 }
 
-export default Configuration
+export default Configuration;
