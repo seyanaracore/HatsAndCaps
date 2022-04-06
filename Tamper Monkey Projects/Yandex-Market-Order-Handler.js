280f78ac3a202d5a2e9211a700d42bff8084a22c
Anton Tigunov <seyanaracore@gmail.com>
2022-04-06 17:34:37 +0300

    Market Orde r Handler

---
+++ Tamper Monkey Projects/Yandex-Market-Order-Handler.js
@@ -0,0 +1,32 @@
+// @name         Yandex Market Order Handler
+// @namespace    Yandex-Market
+// @version      2.0
+// @description  try to take over the world!
+// @author       You
+// @match        https://partner.market.yandex.ru/supplier/*/orders/*
+// @icon         chrome://favicon/https://market.yandex.ru/
+// @require      https://raw.githubusercontent.com/seyanaracore/HatsAndCaps/main/Tamper%20Monkey%20Projects/Yandex-Market-Order-Handler.js
+// @grant        none
+
+const items = document.querySelectorAll(
+   ".style-offerDetailWrapper___3r9k1 span[data-tid='ae445ad4']"
+);
+
+items.forEach((artNumber) => {
+   if (!artNumber.classList.length) {
+      let art = artNumber.textContent.split("");
+
+      art.splice(2, 0, "-");
+      art.splice(6, 0, "-");
+      art.splice(9, 0, "-");
+
+      art = art.join("");
+
+      artNumber.innerText = art;
+      artNumber.style.cursor = "pointer";
+      artNumber.addEventListener("click", (e) => {
+         navigator.clipboard.writeText(e.target.textContent.trim());
+         alert("copied");
+      });
+   }
+});
