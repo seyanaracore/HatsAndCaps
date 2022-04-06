e1758bf5bc75c2d79e147c02675554acf389db8c
Anton Tigunov <seyanaracore@gmail.com>
2022-04-06 10:58:07 +0300

    errors concat links

--- Tamper Monkey Projects/Universal Parser/Project/In/Components/transferErrorLinks.js
+++ Tamper Monkey Projects/Universal Parser/Project/In/Components/transferErrorLinks.js
@@ -4,7 +4,7 @@ import { setItemsLinks } from "./DataSetters"
 const transferErrorLinks = () => {
 	const errorLinks = getErrors()
 	const links = getLinks()
-	if (errorLinks.length) setItemsLinks([...links, ...errorLinks])
+	if (errorLinks.length) setItemsLinks(links.concat(errorLinks))
 }
 
 export default transferErrorLinks
