3c351922c49a7c17e19862b15b0b4ae38d9b2995
Anton Tigunov <seyanaracore@gmail.com>
2022-04-15 16:01:39 +0300

    Refunder fix

--- Tamper Monkey Projects/Wildberries-Cart-Cleaner.js
+++ Tamper Monkey Projects/Wildberries-Cart-Cleaner.js
@@ -8,22 +8,25 @@
 // @require      https://raw.githubusercontent.com/seyanaracore/HatsAndCaps/main/Tamper%20Monkey%20Projects/Wildberries-Cart-Cleaner.js
 // @grant        none
 
-
 const clearCart = async (departureFrom) => {
-	const btns = [...document.querySelectorAll(".btn__del.j-basket-item-del")]
-	const btnsObjects = btns.map(btn => {
-		const departure = btn.parentElement.parentElement.parentElement.childNodes[3].childNodes[3].childNodes[8].firstElementChild.firstElementChild.textContent
-		return { departure, rmButton: btn }
-	})
-	const filteredBtns = departureFrom
-		? btnsObjects.filter((btn) => btn.departure === departureFrom)
-		: btnsObjects
+   const btns = [...document.querySelectorAll(".btn__del.j-basket-item-del")];
+   const btnsObjects = btns.map((btn) => {
+      const departure = departureFrom
+         ? btn.parentElement.parentElement.parentElement.childNodes[3]
+              .childNodes[3].childNodes[8].firstElementChild.firstElementChild
+              .textContent
+         : null;
+      return { departure, rmButton: btn };
+   });
+   const filteredBtns = departureFrom
+      ? btnsObjects.filter((btn) => btn.departure === departureFrom)
+      : btnsObjects;
 
-	for (const btn of filteredBtns) {
-		await window.sleep(0.2)
-		btn.rmButton.click()
-	}
-}
+   for (const btn of filteredBtns) {
+      await window.sleep(0.2);
+      btn.rmButton.click();
+   }
+};
 
 console.log(`clearCart() - чтобы очистить корзину;
 clearCart(departureFrom) - удалить товары из этих места;`);
--- Tamper Monkey Projects/Wildberries-Items-Refunder.js
+++ Tamper Monkey Projects/Wildberries-Items-Refunder.js
@@ -63,9 +63,9 @@ const dataHandler = () => {
 	const errorMsgs = []
 
 	if (!sizes) errorMsgs.push("Нет найдено ни одного размера у товара")
-	if (!sizeToRefund) errorMsgs.push("Не найдено ни 1 размера к возврату у данного SKU: " + getWBSKU)
-	if (!sizes[sizeToRefund]) errorMsgs.push("Не найден нужный размер у данного SKU: " + getWBSKU)
-	if (errorMsgs.length) throw new Error(errorMsgs.join("; "))
+	if (!sizeToRefund) errorMsgs.push("Не найдено ни 1 размера к возврату у данного SKU: " + WBSKU)
+	if (!sizes[sizeToRefund]) errorMsgs.push("Не найден нужный размер у данного SKU: " + WBSKU)
+	if (errorMsgs.length) throw new Error(errorMsgs.join("//"))
 
 	sizes[sizeToRefund].click(); // Выбор нужного размера
 	buyButton.click() //Добавление размера корзину
