8700c38ba888fec524a12a2aa448c389a6689373
Anton Tigunov <seyanaracore@gmail.com>
2022-04-18 17:11:57 +0300

    Lamoda-Items-Checker

---
+++ Tamper Monkey Projects/Lamoda-Items-Checker.js
@@ -0,0 +1,23 @@
+// @name         Lamoda Items Checker
+// @namespace    Lamoda
+// @version      1.0
+// @match        https://www.lamoda.ru/*
+// @icon         chrome://favicon/https://www.lamoda.ru/
+// @description  Lamoda Parser
+// @require      https://raw.githubusercontent.com/seyanaracore/HatsAndCaps/main/Tamper%20Monkey%20Projects/Yandex%20Market%20-%20parser/Project/Out/app.js
+// @require		  https://raw.githubusercontent.com/seyanaracore/HatsAndCaps/main/Tamper%20Monkey%20Projects/Lamoda-Items-Checker.js
+// @grant        none
+
+const config = {
+   validatePageUrl: false,
+};
+
+const dataHandler = () => {
+   const buyButton = document.querySelector(".x-add-to-cart button");
+   const isDisabled = !!buyButton.getAttribute("disabled");
+
+   return { isDisabled };
+};
+
+window.initializeMethods([dataHandler]);
+window.initializeVariables([{ name: "config", value: config }]);
