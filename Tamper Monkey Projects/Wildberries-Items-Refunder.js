7abebed3eece0dc7a18d1b6c2300f31d1db8544b
Anton Tigunov <seyanaracore@gmail.com>
2022-04-21 13:54:37 +0300

    Check buy button

--- Tamper Monkey Projects/Wildberries-Items-Refunder.js
+++ Tamper Monkey Projects/Wildberries-Items-Refunder.js
@@ -11,71 +11,85 @@
 
 const lcKey = "wbItemsToRefund";
 const getSizes = () => {
-	const sizes = {};
-	document.querySelectorAll(".sizes-list__size").forEach((size) => {
-		sizes[size.textContent] = size.parentNode;
-	});
-	return sizes;
+   const sizes = {};
+   document.querySelectorAll(".sizes-list__size").forEach((size) => {
+      sizes[size.textContent] = size.parentNode;
+   });
+   return sizes;
 };
 const setItemsListToRefund = (itemsList) => {
-	return window.LocalStorageUtil.set(lcKey, itemsList);
+   return window.LocalStorageUtil.set(lcKey, itemsList);
 };
 const getItemsListToRefund = () => {
-	return window.LocalStorageUtil.get(lcKey) || [];
+   return window.LocalStorageUtil.get(lcKey) || [];
 };
 const removeItemsListToRefund = () => {
-	window.LocalStorageUtil.delete(lcKey);
-	console.log("Товары к возврату очищены")
+   window.LocalStorageUtil.delete(lcKey);
+   console.log("Товары к возврату очищены");
 };
 const getBuyButton = () =>
-	[...document.querySelectorAll(".btn-main")].find(
-		(btn) =>
-			btn.textContent === "        Добавить в корзину        В корзину    "
-	);
+   [...document.querySelectorAll(".btn-main")].find(
+      (btn) =>
+         btn.textContent === "        Добавить в корзину        В корзину    "
+   );
 const findSize = (SKU) => {
-	const itemsList = getItemsListToRefund();
-	const size = itemsList.find((item) => (item.sku === SKU))?.size || null
-	console.log("Size to refund: " + size)
-	return size;
+   const itemsList = getItemsListToRefund();
+   const size = itemsList.find((item) => item.sku === SKU)?.size || null;
+   console.log("Size to refund: " + size);
+   return size;
 };
-const getWBSKU = () => document.querySelector("#productNmId").textContent
+const getWBSKU = () => document.querySelector("#productNmId").textContent;
 
 const deleteItemSize = (SKU, size) => {
-	const itemsList = getItemsListToRefund();
-	const filteredItemsList = itemsList.filter((item) => {
-		return item.sku !== SKU || item.size !== size
-	})
-	console.log("Осталось товаров: " + filteredItemsList.length, filteredItemsList)
-	setItemsListToRefund(filteredItemsList);
-}
+   const itemsList = getItemsListToRefund();
+   const filteredItemsList = itemsList.filter((item) => {
+      return item.sku !== SKU || item.size !== size;
+   });
+   console.log(
+      "Осталось товаров: " + filteredItemsList.length,
+      filteredItemsList
+   );
+   setItemsListToRefund(filteredItemsList);
+};
 
 const consoleInfo = `getItemsListToRefund() - Получить список товаров;
 setItemsListToRefund([{sku, size}]) - Установить список товаров;
-removeItemsListToRefund() - Очистить список товаров;`
-console.log(consoleInfo)
+removeItemsListToRefund() - Очистить список товаров;`;
+console.log(consoleInfo);
 
 const dataHandler = () => {
-	const buyButton = getBuyButton()
-	const WBSKU = getWBSKU();
-	const sizes = getSizes();
-	const sizeToRefund = findSize(WBSKU);
+   const buyButton = getBuyButton();
+   const WBSKU = getWBSKU();
+   const sizes = getSizes();
+   const sizeToRefund = findSize(WBSKU);
 
-	const errorMsgs = []
+   const errorMsgs = [];
 
-	if (!sizes) errorMsgs.push("Нет найдено ни одного размера у товара")
-	if (!sizeToRefund) errorMsgs.push("Не найдено ни 1 размера к возврату у данного SKU: " + WBSKU)
-	if (!sizes[sizeToRefund]) errorMsgs.push("Не найден нужный размер у данного SKU: " + WBSKU)
-	if (errorMsgs.length) throw new Error(errorMsgs.join(", "))
+   if (buyButton.classList.contains("hide"))
+      errorMsgs.push("Кнопка покупки не доступна: " + WBSKU);
+   if (!sizes) errorMsgs.push("Нет найдено ни одного размера у товара");
+   if (!sizeToRefund)
+      errorMsgs.push(
+         "Не найдено ни 1 размера к возврату у данного SKU: " + WBSKU
+      );
+   if (!sizes[sizeToRefund])
+      errorMsgs.push("Не найден нужный размер у данного SKU: " + WBSKU);
+   if (errorMsgs.length) throw new Error(errorMsgs.join(", "));
 
-	sizes[sizeToRefund].click(); // Выбор нужного размера
-	buyButton.click() //Добавление размера корзину
-	deleteItemSize(WBSKU, sizeToRefund) //Убрать размер к возврату у данного SKU
+   sizes[sizeToRefund].click(); // Выбор нужного размера
+   buyButton.click(); //Добавление размера корзину
+   deleteItemSize(WBSKU, sizeToRefund); //Убрать размер к возврату у данного SKU
 
-	return { WBSKU, status: "ok" }
+   return { WBSKU, status: "ok" };
 };
 const config = {
-	rmDuplicateUrls: false,
-}
+   rmDuplicateUrls: false,
+};
 
-window.initializeVariables([{ name: "config", value: config }])
-window.initializeMethods([dataHandler, getItemsListToRefund, setItemsListToRefund, removeItemsListToRefund]);
+window.initializeVariables([{ name: "config", value: config }]);
+window.initializeMethods([
+   dataHandler,
+   getItemsListToRefund,
+   setItemsListToRefund,
+   removeItemsListToRefund,
+]);
