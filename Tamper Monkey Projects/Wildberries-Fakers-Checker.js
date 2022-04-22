e9e0a7a22c2bcff9679a05e7b850eb3bac35282e
Anton Tigunov <seyanaracore@gmail.com>
2022-04-22 11:16:22 +0300

    Fakers-Checker

---
+++ Tamper Monkey Projects/Wildberries-Fakers-Checker.js
@@ -0,0 +1,27 @@
+// @name         Wildberries Fakers Checker
+// @namespace    Wildberries
+// @version      1.0
+// @description  try to take over the world!
+// @author       You
+// @match        https://www.wildberries.ru/*
+// @icon         chrome://favicon/https://www.wildberries.ru/
+// @require      https://raw.githubusercontent.com/seyanaracore/HatsAndCaps/main/Tamper%20Monkey%20Projects/Universal%20Parser/Project/Out/app.js
+// @require      https://raw.githubusercontent.com/seyanaracore/HatsAndCaps/main/Tamper%20Monkey%20Projects/Wildberries-Fakers-Cheker.js
+// @grant        none
+
+const lcKey = "wbFakersItems";
+
+const getWBSKU = () => document.querySelector("#productNmId").textContent;
+
+const dataHandler = async () => {
+   const WBSKU = getWBSKU();
+   const pageError = document.querySelector("#errorPage");
+
+   await window.sleep(0.5);
+   return { WBSKU, status: pageError ? "blocked" : "notBLocked" };
+};
+const config = {
+   rmDuplicateUrls: false,
+};
+
+window.initializeMethods([dataHandler]);
