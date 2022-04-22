ae27913accc8bc7b7c20c41be378b1451a55d188
Anton Tigunov <seyanaracore@gmail.com>
2022-04-22 11:19:10 +0300

    url fix

--- Tamper Monkey Projects/Wildberries-Fakers-Checker.js
+++ Tamper Monkey Projects/Wildberries-Fakers-Checker.js
@@ -6,7 +6,7 @@
 // @match        https://www.wildberries.ru/*
 // @icon         chrome://favicon/https://www.wildberries.ru/
 // @require      https://raw.githubusercontent.com/seyanaracore/HatsAndCaps/main/Tamper%20Monkey%20Projects/Universal%20Parser/Project/Out/app.js
-// @require      https://raw.githubusercontent.com/seyanaracore/HatsAndCaps/main/Tamper%20Monkey%20Projects/Wildberries-Fakers-Cheker.js
+// @require      https://raw.githubusercontent.com/seyanaracore/HatsAndCaps/main/Tamper%20Monkey%20Projects/Wildberries-Fakers-Checker.js
 // @grant        none
 
 const lcKey = "wbFakersItems";
