b03b3fb5387fc58e5a5813b0f067d7c43856926b
Anton Tigunov <seyanaracore@gmail.com>
2022-04-29 11:56:48 +0300

    Ozon sku copy

---
+++ Tamper Monkey Projects/Ozon-SKU-Copy.js
@@ -0,0 +1,16 @@
+// @name         Ozon SKU Copy
+// @namespace    Ozon
+// @version      1.0
+// @match        https://www.ozon.ru/product/*
+// @icon         chrome://favicon/https://www.ozon.ru/
+// @description  Ozon sku copy func
+// @require		  https://raw.githubusercontent.com/seyanaracore/HatsAndCaps/main/Tamper%20Monkey%20Projects/Ozon-SKU-Copy.js
+// @grant        none
+
+window.sleep(1).then(() => {
+   document.querySelector('[data-widget="webDetailSKU"]').onclick = (e) => {
+      const SKU = e.target.textContent.split(" ")[2];
+      console.log(SKU);
+      window.copyToClipboard(SKU);
+   };
+});
