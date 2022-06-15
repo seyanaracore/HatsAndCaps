fa49277e06370f6385b338f086e30ceffa5114ec
Anton Tigunov <seyanaracore@gmail.com>
2022-06-15 10:16:27 +0300

    handling errors

--- Tamper Monkey Projects/Wigens-Items-Parser.js
+++ Tamper Monkey Projects/Wigens-Items-Parser.js
@@ -10,6 +10,7 @@
 // @grant        none
 
 const $ = (sel) => document.querySelectorAll(sel);
+const someError = "Some went wrong";
 
 const getVariants = () => {
    const variantsList = [...$(".swatch-option.image")];
@@ -51,17 +52,22 @@ const generateProductsFromVariants = (product) => {
 const dataHandler = () => {
    const productData = {};
 
-   productData.art = $('[data-th="Art.no"]')[0].textContent;
-   productData.productName = $(
-      '[data-ui-id="page-title-wrapper"]'
-   )[0].textContent;
-   productData.variants = getVariants();
+   productData.art = $('[data-th="Art.no"]')[0]?.textContent || someError;
+   productData.productName =
+      $('[data-ui-id="page-title-wrapper"]')[0]?.textContent || someError;
+
+   try {
+      productData.variants = getVariants();
+   } catch (err) {
+      productData.variants = someError;
+   }
+
    productData.sizes = [...$(".swatch-option.text")]
-      .map((size) => size.textContent)
+      .map((size) => size?.textContent || someError)
       .join(",");
-   productData.country = $('[data-th="Country of origin"]')[0].textContent;
-   productData.material = $('[data-th="Material"]')[0].textContent;
-   productData.lining = $('[data-th="Lining"]')[0].textContent;
+   productData.country = $('[data-th="Country of origin"]')[0]?.textContent || someError;
+   productData.material = $('[data-th="Material"]')[0]?.textContent || someError;
+   productData.lining = $('[data-th="Lining"]')[0]?.textContent || someError;
 
    const products = generateProductsFromVariants(productData);
 
@@ -69,7 +75,9 @@ const dataHandler = () => {
 };
 
 const downloadItems = () => {
-   const itemsInfo = getItemsData().map(el => el[0]).flat();
+   const itemsInfo = getItemsData()
+      .map((el) => ({ ...el[0], handlingUrl: el.handlingURL }))
+      .flat();
    if (!itemsInfo.length) throw new Error("Items data list is empty.");
 
    window.download(
@@ -77,7 +85,7 @@ const downloadItems = () => {
       "Wigens",
       "csv"
    );
-}
+};
 
 /*
    1. setItemsLinks([])
