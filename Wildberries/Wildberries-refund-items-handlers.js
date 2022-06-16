3431becdb07384e10b3489d64a5c22773224a2b2
Anton Tigunov <seyanaracore@gmail.com>
2022-06-16 13:19:24 +0300

    Wb refund

--- Wildberries/Wildberries-refund-items-handlers.js
+++ Wildberries/Wildberries-refund-items-handlers.js
@@ -14,7 +14,7 @@ window.download(
 //Доставки, вписать нужный слайс
 
 const items = [...document.querySelectorAll(".goods-list-delivery img")]
-   .slice(0, 390)
+   .slice(5,11)
    .map((img) => ({
       sku: img.src.split("/").at(-1).split("-")[0],
       size: img.parentNode.children[4]?.textContent.trim() || "Some went wrong",
