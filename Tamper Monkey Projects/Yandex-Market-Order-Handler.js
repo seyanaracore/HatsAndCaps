152b77d1afdbdf178221dc53fdbb0e351c6d2b40
Anton Tigunov <seyanaracore@gmail.com>
2022-07-05 14:23:29 +0300

    handle multiply order

--- Tamper Monkey Projects/Yandex-Market-Order-Handler.js
+++ Tamper Monkey Projects/Yandex-Market-Order-Handler.js
@@ -7,31 +7,36 @@
 // @icon         chrome://favicon/https://market.yandex.ru/
 // @require      https://raw.githubusercontent.com/seyanaracore/HatsAndCaps/main/Tamper%20Monkey%20Projects/Yandex-Market-Order-Handler.js
 // @grant        none
+const items = getSKUList().map((el) => ({ clicked: false, item: el }));
 
 function setItemsArtHandler() {
-   const items = document.querySelectorAll(
-      "[data-e2e='sku'] span[data-tid='ae445ad4']"
-   );
+   items.forEach((el) => {
+      let art = el.item.textContent.split("");
 
-   items.forEach((artNumber) => {
-      if (!artNumber.classList.length) {
-         let art = artNumber.textContent.split("");
+      art.splice(2, 0, "-");
+      art.splice(6, 0, "-");
+      art.splice(9, 0, "-");
 
-         art.splice(2, 0, "-");
-         art.splice(6, 0, "-");
-         art.splice(9, 0, "-");
+      art = art.join("");
 
-         art = art.join("");
+      artNumber.innerText = art;
+      artNumber.style.cursor = "pointer";
+      artNumber.style.backgroundColor = "yellow";
 
-         artNumber.innerText = art;
-         artNumber.style.cursor = "pointer";
-         artNumber.style.backgroundColor = "yellow";
-         artNumber.addEventListener("click", (e) => {
-            const SKU = e.target.textContent.trim();
-            navigator.clipboard.writeText(SKU);
-            window.notify(SKU + " скопировано");
-         });
-      }
+      const itemOrderCount =
+         artNumber.parentElement.parentElement.parentElement.parentElement
+            .parentElement.parentElement.nextSibling.nextSibling.children[0]
+            .children[0].children[0];
+      const orderedMoreThanOne = parseInt(itemOrderCount.textContent) > 1;
+
+      if (orderedMoreThanOne) itemOrderCount.style.color = "red";
+
+      artNumber.addEventListener("click", (e) => {
+         if (orderedMoreThanOne) alert("Внимание, количество больше 1 штуки");
+         const SKU = e.target.textContent.trim();
+         navigator.clipboard.writeText(SKU);
+         window.notify(SKU + " скопировано");
+      });
    });
 }
 
@@ -68,8 +73,33 @@ function setDataHandler() {
    });
 }
 
+function getSKUList() {
+   return [
+      ...document.querySelectorAll(
+         "[data-e2e='sku'] span[data-tid='ae445ad4']"
+      ),
+   ].filter((el) => !el.classList.length);
+}
+
+function trackCopyArts() {
+   window.addEventListener("beforeunload", (event) => {
+      if (items.find((el) => el.clicked === true)) {
+         const indifference = confirm(
+            "Внимание! Не все артикулы были скопированы."
+         );
+
+         if (!indifference) {
+            event.preventDefault();
+            // Chrome требует установки возвратного значения.
+            event.returnValue = "";
+         }
+      }
+   });
+}
+
 window.sleep(1).then(() => {
    setItemsArtHandler();
    setOrderNumberHandler();
    setDataHandler();
+   trackCopyArts();
 });
