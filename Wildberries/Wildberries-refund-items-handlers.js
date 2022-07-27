51cac6b0f927c7afe34f398d4865b1615a227883
Anton Tigunov <seyanaracore@gmail.com>
2022-07-27 17:27:24 +0300

    WB refund

--- Wildberries/Wildberries-refund-items-handlers.js
+++ Wildberries/Wildberries-refund-items-handlers.js
@@ -13,15 +13,32 @@ window.download(
 
 //Доставки, вписать нужный слайс
 
-const items = [...document.querySelectorAll(".goods-list-delivery img")]
-   .slice(26,44)
-   .map((img) => ({
-      sku: img.src.split("/").at(-4).split("-")[0],
-      size: img.parentNode.children[4]?.textContent.trim() || "Some went wrong",
-   }));
+const getItems = (date, startWith = 0) => {
+   const items = [
+      ...document.querySelectorAll(
+         ".delivery-block__content .goods-list-delivery img"
+      ),
+   ]
+      .map((img) => ({
+         sku: img.src.split("/").at(-4).split("-")[0],
+         size:
+            img.parentNode.children[4]?.textContent.trim() || "Some went wrong",
+         date: img.parentElement.parentElement.parentElement.children[1]
+            .children[0].children[3]?.children[0].textContent,
+         el: img,
+      }))
+      .filter((item) => item.date === date);
+   const slicedItems = items.slice(startWith, items.length);
+   slicedItems.forEach(
+      (item) =>
+         (item.el.parentElement.parentElement.style.border = "2px solid black")
+   );
+   return slicedItems.map((el) => {
+      delete el.date;
+      delete el.el
+      return el;
+   });
+};
 
-window.download(
-   { content: items, headers: "template" },
-   "Браки",
-   "csv"
-);
+const items = getItems("27.07.2022", 1);
+window.download({ content: items, headers: "template" }, "Возврат шляпы 2707", "csv");
