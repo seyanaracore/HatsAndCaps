a7a4cd1a4393158dead9c20db36ccca4b04ae63e
Anton Tigunov <seyanaracore@gmail.com>
2022-11-19 22:10:29 +0300

    fix downloader

--- Tamper Monkey Projects/Wildberries-refund-downloader.js
+++ Tamper Monkey Projects/Wildberries-refund-downloader.js
@@ -2,13 +2,12 @@ const копироватьДанные = () => {
 	const itemsList = [...document.querySelectorAll(".j-open-product-popup")]
 	.map((el) => {
 		 return {
-				SKU: el.getAttribute("data-popup-nm-id"),
-				Размер: el.childNodes[9]?.textContent.trim() || 0,
-				Заказно: el.parentElement.children[1].children[2].children[3].children[1].textContent,
+				SKU: el?.getAttribute("data-popup-nm-id") || 'Ошибка',
+				Размер: el?.childNodes[9]?.textContent.trim() || 0,
+				Заказно: el?.parentElement?.children[1]?.children[2]?.children[3]?.children[1]?.textContent.trim() || 'Ошибка',
 				Доставлено:
-					 el.parentElement.children[1].children[2].children[6].children[3]
-							.textContent,
-				ТипВозврата: el.parentElement.children[1].children[0].children[0].textContent.trim()
+					 el?.parentElement?.children[1]?.children[2]?.children[6]?.children[3]?.textContent.trim() || el.parentElement?.children[1]?.children[2]?.children[6].textContent.trim() || 'Ошибка',
+				ТипВозврата: el?.parentElement?.children[1]?.children[0]?.children[0]?.textContent?.trim() || 'Ошибка'
 		 };
 	})
 
